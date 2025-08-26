912749609c28c15a87157190885418e3715fac1a
Martin Namuche <77213091+namuche@users.noreply.github.com>
2025-08-26 13:09:04 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,11 @@
 // vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+const { defineConfig } = require('vite');
+const react = require('@vitejs/plugin-react');
 
-const SUPABASE_URL = process.env.VITE_SUPABASE_URL || '';
-const SUPABASE_KEY = process.env.VITE_SUPABASE_KEY || '';
-
-export default defineConfig({
+module.exports = defineConfig({
   plugins: [react()],
   define: {
-    'import.meta.env.VITE_SUPABASE_URL': JSON.stringify(SUPABASE_URL),
-    'import.meta.env.VITE_SUPABASE_KEY': JSON.stringify(SUPABASE_KEY),
+    'import.meta.env.VITE_SUPABASE_URL': JSON.stringify(process.env.VITE_SUPABASE_URL),
+    'import.meta.env.VITE_SUPABASE_KEY': JSON.stringify(process.env.VITE_SUPABASE_KEY),
   },
 });
