22c2db31e210853024c73617236d1588ec626489
Martin Namuche <77213091+namuche@users.noreply.github.com>
2025-08-26 13:01:29 -0500

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Lee las variables aquí (Node.js)
-const supabaseUrl = process.env.VITE_SUPABASE_URL || '';
-const supabaseKey = process.env.VITE_SUPABASE_KEY || '';
+// Forzamos la lectura de variables
+const SUPABASE_URL = process.env.VITE_SUPABASE_URL || '';
+const SUPABASE_KEY = process.env.VITE_SUPABASE_KEY || '';
 
 export default defineConfig({
   plugins: [react()],
   define: {
-    'import.meta.env.VITE_SUPABASE_URL': JSON.stringify(supabaseUrl),
-    'import.meta.env.VITE_SUPABASE_KEY': JSON.stringify(supabaseKey),
+    'import.meta.env.VITE_SUPABASE_URL': JSON.stringify(SUPABASE_URL),
+    'import.meta.env.VITE_SUPABASE_KEY': JSON.stringify(SUPABASE_KEY),
+  },
+  // Aseguramos que Vite no falle en modo producción
+  build: {
+    sourcemap: false,
+    minify: true,
   },
 });
