a73717e1377dd096b0a191c000d14313857d8720
Martin Namuche <77213091+namuche@users.noreply.github.com>
2025-08-26 12:52:30 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// Lee las variables aquí (Node.js)
+const supabaseUrl = process.env.VITE_SUPABASE_URL || '';
+const supabaseKey = process.env.VITE_SUPABASE_KEY || '';
+
 export default defineConfig({
   plugins: [react()],
   define: {
-    'import.meta.env.VITE_SUPABASE_URL': JSON.stringify(process.env.VITE_SUPABASE_URL),
-    'import.meta.env.VITE_SUPABASE_KEY': JSON.stringify(process.env.VITE_SUPABASE_KEY),
+    'import.meta.env.VITE_SUPABASE_URL': JSON.stringify(supabaseUrl),
+    'import.meta.env.VITE_SUPABASE_KEY': JSON.stringify(supabaseKey),
   },
 });
