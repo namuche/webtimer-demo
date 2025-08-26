ff10561188e810b4c7d16e1358df87df248a9390
Martin Namuche <77213091+namuche@users.noreply.github.com>
2025-08-26 13:16:43 -0500

    Add files via upload

---
+++ vite.config.cjs
@@ -0,0 +1,15 @@
+// vite.config.cjs
+const { defineConfig } = require('vite');
+const react = require('@vitejs/plugin-react');
+
+module.exports = defineConfig({
+  plugins: [react()],
+  define: {
+    'import.meta.env.VITE_SUPABASE_URL': JSON.stringify(process.env.VITE_SUPABASE_URL),
+    'import.meta.env.VITE_SUPABASE_KEY': JSON.stringify(process.env.VITE_SUPABASE_KEY),
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+});
